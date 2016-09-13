39384dd19a0db2aa5df6e28a4115a30ff2352867
lenriquez <felipe096@gmail.com>
2016-09-13 11:53:48 -0500

    Add keys as object rather than array

--- app/key.component.ts
+++ app/key.component.ts
@@ -1,10 +1,25 @@
 import { Component, Input } from '@angular/core';
 
+export class Key {
+  title: string ;
+  userName: string;
+  url: string;
+  password: string;
+  comments: string;
+}
+
 @Component({
   selector: '[key]',
-  template: `<td *ngFor="let item of row">{{item}}</td>`
+  template: `
+    <td>{{key.title}}</td>
+    <td>{{key.userName}}</td>
+    <td>{{key.url}}</td>
+    <td>{{key.comments}}</td>
+  `
 })
 
 export class KeyComponent {
-  @Input('key') row;
+  @Input('key') key;
 }
+
+
--- app/table.component.ts
+++ app/table.component.ts
@@ -7,13 +7,24 @@ import { Component, Input } from '@angular/core';
       <th *ngFor="let header of headers" class="text-center">
         {{ header }}
       </th>
-      <tr *ngFor="let line of data" [key]="line"></tr>
+      <tr *ngFor="let row of keys" [key]="row"></tr>
     </table>
   `
 })
 
 export class TableComponent {
-  @Input('key-table') data;
+  @Input('key-table') keys;
 
+   keys = [
+    { title: 'Gmail' ;
+      userName: 'felipe096';
+      url: '';
+      password: '******';
+      comments: 'N/A';
+    }
+  ];
+
+  //keys = [ ['Google','felipe096','', ''], ['Facebook','felipe096@gmail.com','www.facebook.com', 'N/A'] ];
+  headers = ['Title', 'UserName', 'URL', 'Comments'];
   constructor() { console.clear(); }
 }
