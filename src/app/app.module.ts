831c4e81e6b60d49de5c61e029f396517e2b18dd
lenriquez <felipe096@gmail.com>
2016-10-29 17:41:07 -0500

    Generate generate key, key-edit and table with angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { KeyComponent } from './key/key.component';
+import { KeyEditComponent } from './key-edit/key-edit.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyComponent,
+    KeyEditComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/key-edit/key-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-key-edit',
+  templateUrl: './key-edit.component.html',
+  styleUrls: ['./key-edit.component.css']
+})
+export class KeyEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
