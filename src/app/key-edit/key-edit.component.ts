e113ecf49e8fc5200a8db9669a8588785c15b2d7
lenriquez <felipe096@gmail.com>
2016-10-29 19:32:43 -0500

    Merge key-edit component with the old one, and adding bootstrap and ng2-bootstrap to the project

--- src/app/key-edit/key-edit.component.ts
+++ src/app/key-edit/key-edit.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'app-key-edit',
   templateUrl: './key-edit.component.html',
   styleUrls: ['./key-edit.component.css']
 })
-export class KeyEditComponent implements OnInit {
 
-  constructor() { }
+export class KeyEditComponent {
+  @ViewChild('smModal') smModal;
 
-  ngOnInit() {
+  key = {}; // TODO: There should be a better way to do this
+  title = '';
+
+  // Display a Modal that will show the properties of a key.
+  showEdit(key) {
+    this.key = key;
+    this.title = 'Editing';
+    this.smModal.show();
   }
 
+  // Display a Modal with all the fields for a key.
+  showNew() {
+    this.title = 'Creating';
+    this.key = {
+      title: '',
+      userName: '',
+      url: '',
+      password: '',
+      comments: ''
+    };
+
+    this.smModal.show();
+    return this.key;
+  }
 }
