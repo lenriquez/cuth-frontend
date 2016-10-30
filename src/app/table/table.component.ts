ee126cc4008c647f774fa9a911c609ae2f154098
lenriquez <felipe096@gmail.com>
2016-10-29 20:09:43 -0500

    Merge table component

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
+export class TableComponent {
+  headers = ['Title', 'UserName', 'URL', 'Comments', 'Actions'];
 
-  constructor() { }
+  keys = [
+    { title: 'Gmail',
+      userName: 'felipe096',
+      url: '',
+      password: '******',
+      comments: 'N/A'
+    }
+  ];
 
-  ngOnInit() {
+  constructor() { console.clear(); }
+
+  addKey(key): void {
+    console.log('Adding new key');
+    this.keys.push(key);
   }
-
 }
